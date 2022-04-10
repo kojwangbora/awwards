3f6ba9444aa436af469eb6507b5c65576a44c8db
kojwangbora <kojwangbora254@gmail.com>
2022-04-10 20:47:11 +0300

    ad static properties

--- awwards/settings.py
+++ awwards/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,6 +120,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+ 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -127,4 +133,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
  
 LOGIN_REDIRECT_URL = 'index'
 
-LOGOUT_REDIRECT_URL = 'login' 
+LOGOUT_REDIRECT_URL = 'index' 
