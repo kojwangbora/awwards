e10885b0a84b50853acff111350f5f95a27cbf50
kojwangbora <kojwangbora254@gmail.com>
2022-04-11 08:35:34 +0300

    add image and profile page for users

--- awwwardss/views.py
+++ awwwardss/views.py
@@ -24,3 +24,7 @@ def signup(request):
     
     return render(request, 'registration/signup.html', {'form': form})
 
+
+    def profile(request, username):
+        return render(request, 'profile.html')
+
