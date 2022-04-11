770c598968117315a1b0530815976ca6be251d4a
kojwangbora <kojwangbora254@gmail.com>
2022-04-11 08:51:16 +0300

    displaying user detals on profile page

--- awwards/urls.py
+++ awwards/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path,include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     re_path('admin/', admin.site.urls),
     re_path('', include('awwwardss.urls'))
     
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
