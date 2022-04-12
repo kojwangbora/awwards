04f23b67a0b407bd7fbfe9ff744c1ab376c1f23f
kojwangbora <kojwangbora254@gmail.com>
2022-04-12 17:05:30 +0300

    add testcase and template files for results and project

--- awwwardss/tests.py
+++ awwwardss/tests.py
@@ -7,7 +7,7 @@ from .models import *
 
 class TestProfile(TestCase):
     def setUp(self):
-        self.user = User(id=1, username='charles', password='wer2345uyq')
+        self.user = User(id=1, username='elly', password='Access')
         self.user.save()
 
     def test_instance(self):
