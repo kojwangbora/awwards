08a6a0878debf5f3279db28b2ea6b4208029d8ba
kojwangbora <kojwangbora254@gmail.com>
2022-04-12 10:59:06 +0300

    add new vitrual environment and reinstall all dependencies for project

--- awwwardss/models.py
+++ awwwardss/models.py
@@ -1,15 +1,15 @@
-from email.policy import default
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
+ 
 from cloudinary.models import CloudinaryField
  
 # Create your models here.
 
 class Profile(models.Model):
-    user =models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    profile_picture=CloudinaryField('images/')
+    user =models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture= CloudinaryField('image', default='preview.png')
     bio= models.TextField(max_length=254, default='My Bio', blank=True)
     name = models.CharField(blank=True, max_length=120)
     location = models.CharField(max_length=60, blank=True)
@@ -32,4 +32,61 @@ class Post(models.Model):
     url = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     technologies = models.CharField(max_length=200)
-    photo = CloudinaryField('image')
+    image = CloudinaryField('image', default='preview.png')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+     
+
+    def __str__(self):
+        return f'{self.title}'
+
+    def delete_post(self):
+        self.delete()
+
+    @classmethod
+    def search_project(cls, title):
+        return cls.objects.filter(title__icontains=title).all()
+
+    @classmethod
+    def all_posts(cls):
+        return cls.objects.all()
+
+    def save_post(self):
+        self.save()
+
+
+
+
+class Rating(models.Model):
+    rating = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (8, '8'),
+        (9, '9'),
+        (10, '10'),
+    )
+
+    design = models.IntegerField(choices=rating, default=0, blank=True)
+    usability = models.IntegerField(choices=rating, blank=True)
+    content = models.IntegerField(choices=rating, blank=True)
+    score = models.FloatField(default=0, blank=True)
+    design_average = models.FloatField(default=0, blank=True)
+    usability_average = models.FloatField(default=0, blank=True)
+    content_average = models.FloatField(default=0, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
+
+    def save_rating(self):
+        self.save()
+
+    @classmethod
+    def get_ratings(cls, id):
+        ratings = Rating.objects.filter(post_id=id).all()
+        return ratings
+
+    def __str__(self):
+        return f'{self.post} Rating'
