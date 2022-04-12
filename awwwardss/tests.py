8a5d8a4366a8c65d2a0724a3e142278bdba94f33
kojwangbora <kojwangbora254@gmail.com>
2022-04-12 16:52:54 +0300

    add testcase for profile

--- awwwardss/tests.py
+++ awwwardss/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
-
+from .models import *
 # Create your tests here.
+ 
+
+
+
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user = User(id=1, username='charles', password='wer2345uyq')
+        self.user.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user, User))
+
+    def test_save_user(self):
+        self.user.save()
+
+    def test_delete_user(self):
+        self.user.delete()
